427ce26024440f47980db886845bce244a35e73a
Jesper Ek <deadbeef84@gmail.com>
2023-05-26 13:41:34 +0200

    WIP

--- app/faster_whisper/core.py
+++ app/faster_whisper/core.py
@@ -1,4 +1,4 @@
-
+import json
 import os
 from typing import BinaryIO, Union
 from io import StringIO
@@ -10,70 +10,31 @@ from .utils import model_converter, ResultWriter, WriteTXT, WriteSRT, WriteVTT, 
 from faster_whisper import WhisperModel
 
 model_name= os.getenv("ASR_MODEL", "base")
-model_path = os.path.join("/root/.cache/faster_whisper", model_name)
-model_converter(model_name, model_path)
+# model_path = os.path.join("/root/.cache/faster_whisper", model_name)
+# model_converter(model_name, model_path)
 
 if torch.cuda.is_available():
-    model = WhisperModel(model_path, device="cuda", compute_type="float16")
+    model = WhisperModel(model_name, download_root="/root/.cache/faster_whisper", device="cuda", compute_type="float16")
 else:
-    model = WhisperModel(model_path, device="cpu", compute_type="int8")
+    model = WhisperModel(model_name, download_root="/root/.cache/faster_whisper", device="cpu", compute_type="int8")
 model_lock = Lock()
 
 def transcribe(
-    audio, 
-    task: Union[str, None], 
+    audio,
+    task: Union[str, None],
     language: Union[str, None],
-    initial_prompt: Union[str, None], 
-    output,
+    initial_prompt: Union[str, None]
 ):
     options_dict = {"task" : task}
     if language:
-        options_dict["language"] = language    
+        options_dict["language"] = language
     if initial_prompt:
-        options_dict["initial_prompt"] = initial_prompt    
-    with model_lock:   
-        segments = []
-        text = ""
-        i = 0
-        segment_generator, info = model.transcribe(audio, beam_size=5, **options_dict)
-        for segment in segment_generator:
-            segments.append(segment)
-            text = text + segment.text
-        result = {
-                "language": options_dict.get("language", info.language),
-                "segments": segments,
-                "text": text,
-            }
-    
-    outputFile = StringIO()
-    write_result(result, outputFile, output)
-    outputFile.seek(0)
-
-    return outputFile
-
-def language_detection(audio):
-    # load audio and pad/trim it to fit 30 seconds
-    audio = whisper.pad_or_trim(audio)
-
-    # detect the spoken language
+        options_dict["initial_prompt"] = initial_prompt
     with model_lock:
-        segments, info = model.transcribe(audio, beam_size=5)
-        detected_lang_code = info.language
-
-    return detected_lang_code
-
-def write_result(
-    result: dict, file: BinaryIO, output: Union[str, None]
-):
-    if(output == "srt"):
-        WriteSRT(ResultWriter).write_result(result, file = file)
-    elif(output == "vtt"):
-        WriteVTT(ResultWriter).write_result(result, file = file)
-    elif(output == "tsv"):
-        WriteTSV(ResultWriter).write_result(result, file = file)
-    elif(output == "json"):
-        WriteJSON(ResultWriter).write_result(result, file = file)
-    elif(output == "txt"):
-        WriteTXT(ResultWriter).write_result(result, file = file)
-    else:
-        return 'Please select an output method!'
+        segments = []
+        i = 0
+        segment_generator, info = model.transcribe(audio, beam_size=5, word_timestamps=True, **options_dict)
+        yield json.dumps({ "language": options_dict.get("language", info.language) })
+        yield "\n"
+        for segment in segment_generator:
+            yield json.dumps({ "start": segment.start, "end": segment.end, "words": [word._asdict() for word in segment.words] }) + "\n"
